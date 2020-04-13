510f2544da34422225e472d4ee275e9c39186109
Pia <pia@geeksfarm.com>
2020-04-13 14:49:47 +0700

    4th commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Homestay from './components/homestay';
+import GoogleMapReact from 'google-map-react';
 
 class App extends Component {
-  render() {
-    const homestay = {
-      "nama": "Dago Homestay",
-      "fotoUrl": "https://www.pegipegi.com/travel/wp-content/uploads/2016/08/Dago-Guest-House-Kampung-Padi-1024x717.jpg",
-      "harga": 500
+  constructor(props) {
+    super(props);
+    this.state = {
+      homestays: []
     };
-    
+  }
+
+  componentDidMount() {
+    fetch("https://raw.githubusercontent.com/algosigma/js-reactjs/master/homestays.json")
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          homestays: data
+        });
+      })
+  }
+
+  render() {
+    let center = {
+      lat: -7.795424,
+      lng: 110.371754
+    }
     return (
       <div className='app'>
         <div className="main">
           <div className="homestays">
-            <Homestay homestay={homestay} />
-            <Homestay homestay={homestay} />
-            <Homestay homestay={homestay} />
+            {this.state.homestays.map((homestay) => {
+                return <Homestay key={homestay.id}
+                          homestay={homestay} />
+            })}
           </div>
         </div>
         <div className="peta">
-
+            <GoogleMapReact
+              center={center}
+              zoom={15}
+            >
+            </GoogleMapReact>
         </div>
       </div>
     ); 
